34355213121aefeb89d0ed3fa0560869d76fa7fa
mhmdkhaz <mhmdkh0957@gmail.com>
2022-03-06 13:08:20 -0800

    add page pricin and blog i should fix resopnsive home and complete page blog

--- js/services.js
+++ js/services.js
@@ -13,28 +13,24 @@ function nav() {
 nav();
 // end nav
 
-// filter element img
-let ImgFilter = document.querySelectorAll(".portfolio-img .box-portfolio");
-let BtnChoose = document.querySelectorAll(".choose ul li");
+/*start CHECK OUR FEATURES*/
+let ElementClick = document.querySelectorAll("#feature ul li");
+let ContentPane = document.querySelectorAll(".tap-pane");
 
-BtnChoose.forEach((btn)=>{
-    btn.addEventListener("click" , FilterImg);
+ElementClick.forEach((element)=>{
+    element.addEventListener("click" , check);
 })
 
-function FilterImg() {
-    ImgFilter.forEach((div)=>{
-        div.style.display = "none";
-        console.log(div);
+function check() {
+    ContentPane.forEach((pane)=>{
+        pane.style.display = "none";
     })
-    document.querySelectorAll(this.dataset.filter).forEach((el) => {
+    document.querySelectorAll(this.dataset.pane).forEach((el) => {
         el.style.display = "block";
     });
-    BtnChoose.forEach((btn)=>{
-        btn.classList.remove("filter-active");
-        this.classList.add("filter-active");
-    })
 }
-// filter element img
+
+/*end CHECK OUR FEATURES*/
 
 // back to top
 let btnToTop = document.querySelector(".backToTop button");
